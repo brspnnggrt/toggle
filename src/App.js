f6ca50ba827357242945f9f254221d21bf36ad0c
Gert <braspenninggert@gmail.com>
2021-09-13 14:10:12 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
 import logo from './logo.svg';
+import { List, Space } from 'antd';
+import React from 'react';
+import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack'
+import { Switch } from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    const id = 'antd-staubli-toggle';
+    this.state = {
+        loading: true,
+        id: id,
+        taskIdRequestData: `${id}-requestData`,
+        data: [],
+    };
+    window.addEventListener("message", this.onMessageReceived, false);
+    this.requestData();
+  }
+
+  onMessageReceived = event => 
+  {
+      if (event.data.taskId === this.state.taskIdRequestData) this.update(event);
+  };
+
+  requestData = () => 
+  {
+      window.parent.postMessage({
+          iframe: this.state.id,
+          taskId: this.state.taskIdRequestData,
+          query: [{
+              api: '/api/rd/v1/Configurator',
+              function: 'getModel',
+              arguments: [{
+                'fakekey': 'fakevalue'
+              }]
+          }],
+          response: [],
+          status: 'request'
+      }, "https://eusb.webcomcpq.com/");
+  };
+
+  update = event => 
+  {
+      // prepare data
+      let model = event.data.response.find(r => r.api === '/api/rd/v1/Configurator' && r.function === 'getModel');
+
+      // update state if data available
+      this.setState({
+          loading: false,
+          data: model.data
+      });
+  };
+  
+  render() {
+    return (
+
+      // this.state.data.Attributes[].Values[].ValueDisplay
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" style={{'height': '100px'}}/>
+          <div className="App">
+            { this.state.loading && 
+              <span>loading data...</span>
+            }
+            {/* { !this.state.loading && 
+              <List dataSource={[...this.state.data.Attributes[0].Values, ...this.state.data.Attributes[1].Values, ...this.state.data.Attributes[2].Values]} 
+              grid={{ gutter: 0, column: 3 }} renderItem={item => (
+                  <List.Item>
+                    <Switch checkedChildren={item.ValueDisplay} unCheckedChildren={item.ValueDisplay} />
+                  </List.Item>
+                )}
+              />
+            } */}
+            { !this.state.loading && 
+              <List dataSource={this.state.data.Attributes} grid={{ gutter: 0, column: 3 }}
+                renderItem={item => (
+                  <List dataSource={item.Values} header={item.Label}
+                  renderItem={inneritem => (
+                    <List.Item>
+                      <Switch checkedChildren={inneritem.ValueDisplay} unCheckedChildren={inneritem.ValueDisplay} />
+                    </List.Item>
+                  )}
+                />
+                )}
+              />
+            }
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
