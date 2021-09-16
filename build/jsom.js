d449eaab562dceffa19d7bd85614e155b6314e5a
Gert <braspenninggert@gmail.com>
2021-09-16 15:50:02 +0200

    New jsom update

--- build/jsom.js
+++ build/jsom.js
@@ -651,7 +651,7 @@
         }));
         request.status = 'response';
         let iframe = document.getElementById(request.iframe);
-        iframe.contentWindow.postMessage(request, "https://localhost:3000/");
+        iframe.contentWindow.postMessage(request, "https://brspnnggrt.github.io/");
     });
 
 })();
