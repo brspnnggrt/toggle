dee6b12bb22c402d46993fea2ef7e027bd2843d3
Gert <braspenninggert@gmail.com>
2021-09-19 19:34:41 +0200

    Update

--- public/jsom.js
+++ public/jsom.js
@@ -630,6 +630,19 @@
             }
         }
     };
+    const getBase64Image = (selector) => {
+        // Create an empty canvas element
+        img = document.querySelector(selector);
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+    
+        // Copy the image contents to the canvas
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        var dataURL = canvas.toDataURL("image/png");
+        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    }
     window.addEventListener("message", async (message) => {
 
         if (message.data.runScript) {
